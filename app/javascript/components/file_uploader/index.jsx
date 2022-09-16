3b54a5194741fe4ad0c1aac411d5951081950620
Bill Reed <billnreed@gmail.com>
2022-09-16 16:08:12 -0400

    file rename

--- app/javascript/components/file_uploader/index.jsx
+++ app/javascript/components/file_uploader/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import styles from "./file_uploader.module.css";
+import styles from "./styles.module.css";
 
 export const FileUploader = ({ text, onUpload }) => {
   const onChange = (e) => {
